4aaabb9b1242c181c15d7c891b1bcb248e9ecd1d
evahcweep-dordinn <78381897+evahcweep-dordinn@users.noreply.github.com>
2021-06-24 23:48:22 +0100

    Update cfg.jsdoc.js

--- src/data/cfg.jsdoc.js
+++ src/data/cfg.jsdoc.js
@@ -15,7 +15,7 @@
  * @property {string} [closeMessage]
  * @property {boolean} [allowUserClose=false]
  * @property {string} [newThreadCategoryId]
- * @property {string} [mentionRole="857745721205719040"]
+ * @property {string} [mentionRole="here"]
  * @property {boolean} [pingOnBotMention=true]
  * @property {string} [botMentionResponse]
  * @property {array} [inboxServerPermission=[]]
